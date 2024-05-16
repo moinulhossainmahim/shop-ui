8f077a96f96c1605c25b706c7520fc0382bf5923
mhossain2 <moinulhossainmahim@gmail.com>
2024-05-16 16:40:26 +0600

    feat: added token fetching from local storage when app is initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
 import Router from "./components/Router"
 import { ToastContainer } from 'react-toastify';
+import { useDispatch } from "react-redux";
 
 import 'react-toastify/dist/ReactToastify.css';
+import { setAuthData } from "./redux/reducers/auth";
+import { SagaActions } from "./redux/sagas/actions";
 
 function App() {
+  const dispatch = useDispatch();
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    if (token) {
+      dispatch(setAuthData({
+        isAuthenticated: true,
+        token,
+        message: 'Sign in successfully',
+      }))
+      dispatch({ type: SagaActions.FetchProfile });
+      dispatch({ type: SagaActions.FetchOrders });
+    }
+  }, [dispatch, token])
+
   return (
     <>
       <ToastContainer />
